7db788adfd46416de02620f27e8224acc51a1603
Felipe Martins <flpmartins88@gmail.com>
2022-07-04 18:30:45 -0300

    Fix order

--- verify.groovy
+++ verify.groovy
@@ -3,6 +3,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+def baseUrl = "http://kernel.ubuntu.com/~kernel-ppa/mainline/"
 
 def currentVersion
 def versionList = []
@@ -18,7 +19,7 @@ if (args) {
 println "Versão atual: ${currentVersion.get()}"
 println "Obtendo lista"
 
-Document doc = Jsoup.connect("http://kernel.ubuntu.com/~kernel-ppa/mainline/").timeout(60000).get();
+Document doc = Jsoup.connect(baseUrl).timeout(60000).get();
 Elements elements = doc.select("body > table > tbody > tr");
 
 if (elements.isEmpty()) {
@@ -26,13 +27,14 @@ if (elements.isEmpty()) {
     return
 }
 
-def version, releaseDate
+def version, releaseDate, href
 
 println "Procurando novas versões"
 
 for (int linha = 0; linha < elements.size(); linha++) {
 
     version     = elements.get(linha).select("td:nth-child(2) > a").text()
+    href        = elements.get(linha).select("td:nth-child(2) > a").attr("href")
     releaseDate = elements.get(linha).select("td:nth-child(3)").text()
 
     // Tratar as versões
@@ -43,28 +45,36 @@ for (int linha = 0; linha < elements.size(); linha++) {
         continue
 
     // println version + " " + version.indexOf("-")
-    version = version.substring(0, version.indexOf("-"))
+    if (version.contains("-"))
+        version = version.substring(0, version.indexOf("-"))
 
     version = version.replace("/", "")
     version = version.replace("v", "")
 
-    versionList += [version:new Version(version), releaseDate: releaseDate]
+    href = baseUrl + href
+
+    versionList += [version: new Version(version), releaseDate: releaseDate, href: href]
 
     //println "Version: ${version} Release Date: ${releaseDate}"
 }
 
 def newVersion
 
-versionList.each {
-    if (it.version.compareTo(currentVersion) == 1) {
-        newVersion = it
-
-        // se dar um break aqui ele vai parar de procurar e vai mostrar somente a próxima, não a mais recente no caso de mais de uma
+versionList
+    .sort { 
+        it.version
     }
+    .each {
+        if (it.version.compareTo(currentVersion) == 1) {
+            newVersion = it
+
+            // se dar um break aqui ele vai parar de procurar e vai mostrar somente a próxima, não a mais recente no caso de mais de uma
+        }
 }
 
 if (newVersion) {
     println "Nova versão: ${newVersion.version.get()} lançada em: ${newVersion.releaseDate}"
+    println "URL: ${newVersion.href}"
 } else {
     println "Não existe nenhuma nova versão"
 }
